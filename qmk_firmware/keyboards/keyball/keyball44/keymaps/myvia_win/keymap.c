febf2c4728fec3db838e4cbeb916aa1b07e5d3cf
kavfmg <takumi50909a@gmail.com>
2026-06-08 10:49:37 +0900

    delete some undefined keys

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia_win/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia_win/keymap.c
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,       KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     SSNP_VRT ,  KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , _______   ,       KC_PGUP  , KC_LEFT  , KC_DOWN    , KC_UP  , KC_RGHT  , KC_F12   ,
     SSNP_HOR ,  JP_COLN , _______  , _______ , _______  , _______  ,       KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
-              _______  , _______ , _______  ,  _______  , _______  ,         GO_PREVIOUS_WINDOW  , GO_NEXT_WINDOW  , _______       , _______  , _______
+              _______  , _______ , _______  ,  _______  , _______  ,         _______  , _______  , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
     _______  , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),               S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , _______  ,
     _______  , KC_1,    KC_2,   KC_3,     KC_4,     KC_5,                 KC_6    , KC_7      , KC_8     , KC_9     , KC_0      , _______,
-    _______  , _______,  _______   , _______   ,_______    ,_______,      KE_ENT,KE_LEFT,KC_DOWN ,KC_UP,KC_RGHT,_______,
+    _______  , _______,  _______   , _______   ,_______    ,_______,      KC_ENT,KC_LEFT,KC_DOWN ,KC_UP,KC_RGHT,_______,
                   KC_0     , KC_DOT  , _______, _______  , _______  ,          KC_DEL   , _______  , _______       , _______  , _______
   ),
 
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // dvorak > 1
   [5] = LAYOUT_universal(
   SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5   ,       KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , NEXT_ITERM   ,       KC_PGUP  , KC_LEFT  , KC_DOWN    , KC_UP  , KC_RGHT  , KC_F12   ,
+    SSNP_VRT ,  KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , _______   ,       KC_PGUP  , KC_LEFT  , KC_DOWN    , KC_UP  , KC_RGHT  , KC_F12   ,
     SSNP_HOR ,  _______ , _______  , _______ , _______  , _______  ,       KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
-              _______  , _______ , _______  ,  _______  , _______  ,         GO_PREVIOUS_WINDOW  , GO_NEXT_WINDOW  , _______       , _______  , _______
+              _______  , _______ , _______  ,  _______  , _______  ,         _______  , _______  , _______       , _______  , _______
 ),
 // dvorak > 2
   [6] = LAYOUT_universal(
